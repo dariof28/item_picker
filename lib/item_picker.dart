71968fe8b1a6fb0ba0293e61a04d376c7d739022
Dario Finocchiaro <dariofinocchiaro93+dev@gmail.com>
2021-04-11 23:06:10 +0200

    style: run dartfmt

--- lib/item_picker.dart
+++ lib/item_picker.dart
@@ -1,5 +1,5 @@
 /// Item picker widgets to build a widget with selectable list items
 library item_picker;
 
-export 'src/single_item_pick.dart';
 export 'src/multiple_item_picker.dart';
+export 'src/single_item_pick.dart';
